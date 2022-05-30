3b5d37c7837f756170f0aa3e4729482d64131668
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2022-05-30 16:27:09 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,26 +11,34 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("=== TEST 2: seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		list.forEach(System.out::println);
-		
+
 		System.out.println("=== TEST 3: seller findAll ===");
 		List<Seller> list2 = sellerDao.findAll();
 		list2.forEach(System.out::println);
-		
-		/*System.out.println("=== TEST 4: seller insert ===");
-		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());*/
+
+		/*
+		 * System.out.println("=== TEST 4: seller insert ==="); Seller newSeller = new
+		 * Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		 * sellerDao.insert(newSeller); System.out.println("Inserted! New id = " +
+		 * newSeller.getId());
+		 */
+
+		System.out.println("=== TEST 5: seller update ===");
+		Seller seller2 = sellerDao.findById(1);
+		seller2.setName("Martha Waine");
+		sellerDao.update(seller2);
+		System.out.println("Update completed!");
 	}
 
 }
